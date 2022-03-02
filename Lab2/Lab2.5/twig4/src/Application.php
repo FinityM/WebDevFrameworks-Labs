364a1caab5af3065794467cc08cf99d9b6a708e7
FinityM <B00125089@mytudublin.ie>
2022-03-02 16:12:12 +0000

    Completed twig 4

---
+++ Lab2/Lab2.5/twig4/src/Application.php
@@ -0,0 +1,42 @@
+<?php
+namespace Tudublin;
+
+class Application
+{
+    const PATH_TO_TEMPLATES = __DIR__ . '/../templates';
+
+    private $mainController;
+
+    public function __construct()
+    {
+        $twig = new \Twig\Environment(new \Twig\Loader\FilesystemLoader(self::PATH_TO_TEMPLATES));
+        $this->mainController = new MainController($twig);
+
+    }
+
+    public function run()
+    {
+        $action = filter_input(INPUT_GET, 'action');
+
+        switch($action) {
+            case 'contact':
+                $this->mainController->contact();
+                break;
+            case 'jokes':
+                $this->mainController->jokes();
+                break;
+
+            case 'about':
+                $this->mainController->about();
+                break;
+
+            case 'products':
+                $this->mainController->products();
+                break;
+
+            case 'index':
+            default:
+                $this->mainController->index();
+        }
+    }
+}
